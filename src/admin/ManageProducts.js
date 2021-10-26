83f7f0386b93bb0058b94f6adfba623f076baa85
Harrish Dhaithya <harrishdhaithya@gmail.com>
2021-10-26 17:28:09 +0530

    commit

--- src/admin/ManageProducts.js
+++ src/admin/ManageProducts.js
@@ -41,12 +41,12 @@ const ManageProducts = () => {
                     return (
                     <div key={index} className="row text-center mb-2 ">
                         <div className="col-4">
-                        <h3 className="text-white text-left">{product.name}</h3>
+                            <h3 className="text-white text-left">{product.name}</h3>
                         </div>
                         <div className="col-4">
                         <Link
                             className="btn btn-success"
-                            to='/admin/product/update/productId'
+                            to={'/admin/product/update/'+product._id}
                         >
                             <span className="">Update</span>
                         </Link>
---
+++ src/admin/UpdateProduct.js
@@ -0,0 +1,181 @@
+import React,{useState,useEffect} from 'react';
+import Base from '../core/Base';
+import { Link } from 'react-router-dom';
+import { createProduct, getCategories, getProduct, updateProduct } from './helper/adminapicall';
+import { isAuthenticated } from '../auth/helper';
+
+
+const UpdateProduct = () => {
+
+    const {user,token} = isAuthenticated();
+    const [values, setValues] = useState({
+        name:"",
+        description:"",
+        price:"",
+        stock:"",
+        photo:"",
+        categories:[],
+        category:"",
+        loading:false,
+        error:"",
+        createProduct:"",
+        getRedirect:false,
+        success:false,
+        formData:new FormData()
+
+    });
+    const {name,description,price,stock,categories,category,loading,error,formData,success}=values;
+    
+    const preload = (productId) =>{
+      getProduct(productId)
+      .then(data=>{
+        if(data.error){
+          setValues({...values,error:data.error});
+        }else{
+          setValues({
+              ...values,
+              name:data.name,
+              description:data.description,
+              price:data.price,
+              category:data.category,
+              formData:new FormData()
+            });
+          console.log(categories);
+        }
+      })
+    }
+    useEffect(() => {
+      preload()
+    }, []);
+
+    //work on it
+    const onSubmit = (event)=>{
+      setValues({...values,error:"",loading:true});
+      updateProduct(user._id,token,formData)
+      .then(data=>{
+        if(data.error){
+          setValues({...values,error:data.error})
+        }else{
+          setValues(
+            {
+            ...values,
+            name:"",
+            description:"",
+            price:"",
+            photo:"",
+            stock:"",
+            loading:false,
+            createProduct:data.name,
+            success:true
+          }
+            )
+
+        }
+      })
+    }
+    const handleChange=name=>event=>{
+        console.log(name);
+        const value = name === "photo"?event.target.files[0]:event.target.value;
+        formData.set(name,value)
+        setValues({...values,[name]:value})
+    }
+    const successMessage = () =>(
+      <div className="alert alert-success mt-3 text-center" style={{display:createProduct?"":"none"}}>
+        <h4>{createProduct} created Successfully</h4>
+      </div>
+    )
+    const sleep = async(milliseconds) => {
+      return new Promise(resolve => setTimeout(resolve, milliseconds))
+    }    
+    const createProductForm = () => (
+        <form >
+          <span>Post photo</span>
+          <div className="form-group">
+            <label className="btn btn-block btn-success">
+              <input
+                onChange={handleChange("photo")}
+                type="file"
+                name="photo"
+                accept="image"
+                placeholder="choose a file"
+              />
+            </label>
+          </div>
+          <div className="form-group">
+            <input
+              onChange={handleChange("name")}
+              name="photo"
+              className="form-control"
+              placeholder="Name"
+              value={name}
+            />
+          </div>
+          <div className="form-group">
+            <textarea
+              onChange={handleChange("description")}
+              name="photo"
+              className="form-control"
+              placeholder="Description"
+              value={description}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              onChange={handleChange("price")}
+              type="number"
+              className="form-control"
+              placeholder="Price"
+              value={price}
+            />
+          </div>
+          <div className="form-group">
+            <select
+              onChange={handleChange("category")}
+              className="form-control"
+              placeholder="Category"
+            >
+              <option>Select</option>
+              {
+                categories&&
+                categories.map((cate,index)=>(
+                  <option key={index} value={cate._id}>{cate.name}</option>
+                ))
+              }
+            </select>
+          </div>
+          <div className="form-group">
+            <input
+              onChange={handleChange("stock")}
+              type="number"
+              className="form-control"
+              placeholder="Quantity"
+              value={stock}
+            />
+          </div>
+          
+          <button type="submit" onClick={onSubmit} className="btn btn-outline-success">
+            Create Product
+          </button>
+        </form>
+      );
+
+
+    
+    return (
+        <Base 
+         title="Add Some Cool Products Here 😍😍😍"
+         description="WELCOME TO PRODUCT CREATION SECTION"
+         className="container bg-light rounded p-4"
+         >
+            <Link to="/admin/dashboard" className="btn btn-md btn-danger mb-2">To Admin Home</Link>
+            <div className="row bg-dark text-white rounded pb-3 pt-3">
+                <div className="col-md-8 offset-md-2">
+                    {successMessage()}
+                    {createProductForm()}
+                </div>
+            </div>
+        </Base>
+    );
+}
+
+export default UpdateProduct;
